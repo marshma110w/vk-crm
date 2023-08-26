92b6eb1ab4280eb538d556b843c44312166f9df5
Michael <m.chichunov@erlyvideo.org>
2023-08-27 01:40:24 +0300

    add confirmation controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  post 'confirmation/confirm'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   root 'hello#show'
