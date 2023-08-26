1b29955ed2ad1f0846d6e2539fba0c155661db90
Max Kirillov <max79kirillov@gmail.com>
2023-08-27 00:11:05 +0300

    Добавил конфиг рубокопа и исправил замечания

--- config/application.rb
+++ config/application.rb
@@ -1,16 +1,18 @@
-require_relative "boot"
+# frozen_string_literal: true
 
-require "rails"
+require_relative 'boot'
+
+require 'rails'
 # Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "active_storage/engine"
-require "action_controller/railtie"
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'active_storage/engine'
+require 'action_controller/railtie'
 # require "action_mailer/railtie"
 # require "action_mailbox/engine"
 # require "action_text/engine"
-require "action_view/railtie"
+require 'action_view/railtie'
 # require "action_cable/engine"
 # require "rails/test_unit/railtie"
 
@@ -28,7 +30,7 @@ module VkCrm
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    config.time_zone = "Moscow"
+    config.time_zone = 'Moscow'
     config.active_record.default_timezone = :local
     # config.eager_load_paths << Rails.root.join("extras")
 
