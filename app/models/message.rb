386752b882436f5e0ed7a4ca03dcade85858be2a
Michael <m.chichunov@erlyvideo.org>
2023-08-27 03:51:45 +0300

    добавил обработку для события message_new

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class Message < ApplicationRecord
+  class << self
+    def create_from_vk(params)
+      Message.create(client_id: params[:from_id], body: params[:text])
+    end
+  end
 end
