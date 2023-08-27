784aee09a85586f373f229c9b9d0c2175d325103
Max Kirillov <max79kirillov@gmail.com>
2023-08-27 03:10:36 +0300

    Первые Vue компоненты

---
+++ app/frontend/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="app">
+  
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      posts: [
+        { id: 1, title: 'Cucumbers', body: 'Info about cucumbers'},
+        { id: 2, title: 'Cucumbers 2', body: 'Info about cucumbers 2'},
+        { id: 3, title: 'Cucumbers 3', body: 'Info about cucumbers 3'}
+      ],
+      title: '',
+      body: ''
+    }
+  },
+
+  methods: {
+    createPost() {
+      const newPost = {
+        id: Date.now(),
+        title: this.title,
+        body: this.body
+      }
+      this.posts.push(newPost)
+      this.title = ''
+      this.body = ''
+    }
+  }
+
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.app {
+  padding: 20px 30px
+}
+</style>
---
+++ app/frontend/components/PostList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="post" v-for="post in posts">
+    <div>Название: {{ post.title }}</div>
+    <div>Описание: {{ post.body}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+
+.post  {
+  padding: 20px;
+  margin-top: 15px;
+  border: 2px solid rebeccapurple;
+  border-radius: 10px;
+}
+</style>
