f23504f09181bc4bbf6c82f982f18592cc108c85
Maxim Kirillov <92055505+noMercy-q@users.noreply.github.com>
2023-08-30 01:09:27 +0300

    Фикс маршрута (#17)

--- app/controllers/front/messages_controller.rb
+++ app/controllers/front/messages_controller.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
-class MessagesController < ActionController::API
-  def list
-    render json: { messages: Message.last(10) }
+module Front
+  class MessagesController < ActionController::API
+    def list
+      render json: { messages: Message.last(10) }
+    end
   end
 end
